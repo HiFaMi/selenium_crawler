b469e503eb2e265016f72814b8d94b34c92cf554
mingyu <rlaalsrbgk@gmail.com>
2020-02-13 21:18:30 +0900

    instagram, twitter login 구현

--- selenium_crawling.py
+++ selenium_crawling.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-def sns_login(email, password):
+def instagram_login(email, password):
     usr_email = email
     usr_password = password
 
@@ -26,5 +29,30 @@ def sns_login(email, password):
 
     click_button = driver.find_element_by_css_selector("div.mt3GC > button.aOOlW.HoLwm")
     click_button.click()
+
+    return driver
+
+
+def twitter_login(email, password):
+    usr_email = email
+    usr_password = password
+
+    path = "/usr/local/bin/chromedriver"
+    driver = webdriver.Chrome(path)
+    driver.get("https://twitter.com")
+
+    wait = WebDriverWait(driver, 10)
+
+    login_elem = wait.until(EC.element_to_be_clickable(
+        (By.XPATH,
+         """//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]/div""")))
+    login_elem.click()
+
+    emei = driver.find_element_by_name("session[username_or_email]")
+    emei.send_keys(usr_email)
+
+    emei = driver.find_element_by_name("session[password]")
+    emei.send_keys(usr_password)
+    emei.send_keys(Keys.RETURN)
 
     return driver
