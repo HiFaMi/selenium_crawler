2cc76de8b975c0a379cee773742cadd6c866d6fe
mingyu <rlaalsrbgk@gmail.com>
2020-02-13 20:43:50 +0900

    email, password 보안

--- selenium_img_down.py
+++ selenium_img_down.py
@@ -1,14 +1,22 @@
-
-
+import os
+import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium_crawling import instagram_login
+from selenium_crawling import sns_login
 
-driver = instagram_login()
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_DIR = os.path.join(ROOT_DIR, ".secret")
+
+secrets = json.load(open(os.path.join(SECRET_DIR, "secrets.json")))
+
+email = secrets["INSTAGRAM_EMAIL"]
+password = secrets["INSTAGRAM_PASSWORD"]
+driver = sns_login(email, password)
 
 driver.implicitly_wait(10)
 search_box = driver.find_element_by_css_selector(
@@ -21,6 +29,5 @@ element = wait.until(EC.element_to_be_clickable(
     (By.XPATH,
      """//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div/a[1]""")))
 
-# search_box.send_keys(Keys.RETURN)
 element.click()
 
