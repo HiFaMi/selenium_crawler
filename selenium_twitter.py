57b7c3ea1d35e6cb1efc08b2e90f503134ed5c8d
mingyu <rlaalsrbgk@gmail.com>
2020-02-16 15:56:07 +0900

    save and load cookie를 사용하여 login 유지

--- selenium_twitter.py
+++ selenium_twitter.py
@@ -1,11 +1,12 @@
 import os
 import json
+import pickle
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 
-from selenium_crawling import twitter_login
+from selenium_crawling import twitter_login, save_and_load_cookie
 from selenium.webdriver.support import expected_conditions as EC
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -29,16 +30,16 @@ search_box = driver.find_element_by_xpath(
 
 search_box.send_keys("#박보영")
 
-wait = WebDriverWait(driver, 10)
-
-# element = wait.until(EC.element_to_be_clickable(
-#     (By.CSS_SELECTOR,
-#      "div > div.css-1dbjc4n:nth-child(2) > div.r-my5ep6.r-rull8r.r-qklmqi.r-6dt33c.r-o7ynqc.r-1j63xyz.css-18t94o4.css-1dbjc4n")))
-
 element = driver.find_element_by_css_selector(
     "div > div.css-1dbjc4n:nth-child(3) > div.r-my5ep6.r-rull8r.r-qklmqi.r-6dt33c.r-o7ynqc.r-1j63xyz.css-18t94o4.css-1dbjc4n > div"
 )
 print(element.text)
 element_list = element.text.split('@')
-change_url = "https://twitter.com/"+ str(element_list[-1])
-driver.get(change_url)
+change_url = "https://twitter.com/" + str(element_list[-1])
+
+save_and_load_cookie(driver=driver, change_url=change_url)
+
+driver.refresh()
+# change_to_media = driver.find_element_by_css_selector(
+#     "nav > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-tzz3ar.r-1pi2tsx.r-hbs49y > div:nth-child(3) > a")
+# change_to_media.click()
