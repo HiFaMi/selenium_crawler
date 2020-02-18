61c4639a59518e278dc1dcc8d1b2f5662a95c7db
mingyu <rlaalsrbgk@gmail.com>
2020-02-18 16:17:52 +0900

    twitter img download 구현

--- selenium_twitter.py
+++ selenium_twitter.py
@@ -1,13 +1,8 @@
 import os
 import json
-import pickle
+import time
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-
-from selenium_crawling import twitter_login, save_and_load_cookie
-from selenium.webdriver.support import expected_conditions as EC
+from selenium_crawling import twitter_login, save_and_load_cookie, download_twitter_image
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_DIR = os.path.join(ROOT_DIR, ".secret")
@@ -17,6 +12,7 @@ secrets = json.load(open(os.path.join(SECRET_DIR, "secrets.json")))
 email = secrets["TWITTER_EMAIL"]
 password = secrets["TWITTER_PASSWORD"]
 
+# twitter login
 driver = twitter_login(email, password)
 
 
@@ -28,18 +24,45 @@ search_box = driver.find_element_by_xpath(
     /div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div[2]/input"""
 )
 
+# 검색
 search_box.send_keys("#박보영")
 
 element = driver.find_element_by_css_selector(
     "div > div.css-1dbjc4n:nth-child(3) > div.r-my5ep6.r-rull8r.r-qklmqi.r-6dt33c.r-o7ynqc.r-1j63xyz.css-18t94o4.css-1dbjc4n > div"
 )
-print(element.text)
+
 element_list = element.text.split('@')
 change_url = "https://twitter.com/" + str(element_list[-1])
 
+# cookie save and load
 save_and_load_cookie(driver=driver, change_url=change_url)
 
 driver.refresh()
-# change_to_media = driver.find_element_by_css_selector(
-#     "nav > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-tzz3ar.r-1pi2tsx.r-hbs49y > div:nth-child(3) > a")
-# change_to_media.click()
+change_to_media = driver.find_element_by_css_selector(
+    "nav > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-tzz3ar.r-1pi2tsx.r-hbs49y > div:nth-child(3) > a")
+change_to_media.click()
+
+time.sleep(2)
+find_imgs = driver.find_elements_by_css_selector("div > img")
+download_twitter_image(imgs_element=find_imgs)
+
+# # set loading time
+# SCROLL_PAUSE_TIME = 2
+#
+# # Get scroll height
+# last_height = driver.execute_script("return document.body.scrollHeight")
+#
+# while True:
+#     # Scroll down to bottom
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#
+#     # Wait to load page
+#     time.sleep(SCROLL_PAUSE_TIME)
+#
+#     download_twitter_image(imgs_element=find_imgs)
+#
+#     # Calculate new scroll height and compare with last scroll height
+#     new_height = driver.execute_script("return document.body.scrollHeight")
+#     if new_height == last_height:
+#         break
+#     last_height = new_height
