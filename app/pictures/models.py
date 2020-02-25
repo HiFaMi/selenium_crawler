8bbfa10b166acbbf82c8f8196ccad7a327127369
mingyu <rlaalsrbgk@gmail.com>
2020-02-25 18:32:40 +0900

    model updat_to 수정

--- app/pictures/models.py
+++ app/pictures/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class PostPicture(models.Model):
     post_user = models.CharField(max_length=150)
-    post_picture = models.ImageField(upload_to="images")
+    post_picture = models.ImageField(upload_to="img")
