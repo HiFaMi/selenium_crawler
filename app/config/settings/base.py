af4ba396302a5f9a04cd1803c3f918078e9baa4e
mingyu <rlaalsrbgk@gmail.com>
2020-02-26 20:24:40 +0900

    staticfiles_dir 설정

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -29,6 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_ROOT = os.path.join(BASE_DIR, '.static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, '.media')
 MEDIA_URL = '/media/'
 
