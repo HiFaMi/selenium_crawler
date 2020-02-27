bb651e4de4b8a7ebb99399d8ac8c9fc8345f54e8
mingyu <rlaalsrbgk@gmail.com>
2020-02-27 16:35:48 +0900

    pagination 구현

--- app/pictures/views/post_list.py
+++ app/pictures/views/post_list.py
@@ -1,10 +1,26 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 from ..models import PostPicture
 
 
 def post_list(request):
-    posts = PostPicture.objects.all()
+    posts_list = PostPicture.objects.all()
+
+    # show 21 contacts per page
+    paginator = Paginator(posts_list, 21)
+
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     context = {
         'posts': posts,
     }
