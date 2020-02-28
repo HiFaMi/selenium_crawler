6f4737a3dcb4f88a1a6ac9eebaea6920fbc75d21
mingyu <rlaalsrbgk@gmail.com>
2020-02-28 18:51:18 +0900

    members url 연결

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,10 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('members/', include('members.urls')),
     path('posts/', include('pictures.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
