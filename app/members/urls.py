23cfb6a2fcea2c2936c4bb7b00b87b3e0ef92846
mingyu <rlaalsrbgk@gmail.com>
2020-02-29 17:22:45 +0900

    login logout url 추가

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views.auth import signup
+from .views.auth_logout import user_logout
+from .views.auth_login import user_login
+from .views.auth_signup import signup
 
+app_name = 'members'
 urlpatterns = [
-    path('signup/', signup, name='sign-up')
-
+    path('signup/', signup, name='signup'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
