6fbfc86c0ed4f3c9693dc489f5332ac0877e8dc8
mingyu <rlaalsrbgk@gmail.com>
2020-03-01 17:04:24 +0900

    like url 설정

--- app/pictures/urls.py
+++ app/pictures/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import post_list
+from .views import post_list, post_like
 
 app_name = 'posts'
 urlpatterns = [
     path('', post_list, name='post-list'),
-
+    path('<int:pk>/like', post_like, name='post-like'),
 
 ]
