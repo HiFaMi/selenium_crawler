26c864314d1b8939b598345541f9032ed91d9357
mingyu <rlaalsrbgk@gmail.com>
2020-03-01 15:46:41 +0900

    post_modal_target 추가

--- app/pictures/models.py
+++ app/pictures/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class PostPicture(models.Model):
     post_user = models.CharField(max_length=150)
+    post_modal_target = models.CharField(max_length=100, blank=True)
     post_picture = models.ImageField(upload_to="img")
