1c86e04695aec6139587c562f719d6d328a0a3a5
mingyu <rlaalsrbgk@gmail.com>
2020-03-01 17:00:09 +0900

    likes 기능 구현

--- app/pictures/models.py
+++ app/pictures/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,3 +6,10 @@ class PostPicture(models.Model):
     post_user = models.CharField(max_length=150)
     post_modal_target = models.CharField(max_length=100, blank=True)
     post_picture = models.ImageField(upload_to="img")
+    post_likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like')
+
+    def __str__(self):
+        return self.post_modal_target
+
+    def total_likes(self):
+        return self.post_likes.count()
