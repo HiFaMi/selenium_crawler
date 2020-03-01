b12981490a94ba7b5c4be56aa74f6a93d0253dc5
mingyu <rlaalsrbgk@gmail.com>
2020-03-01 21:56:46 +0900

    post like use ajax 구현

--- app/pictures/views/post_like.py
+++ app/pictures/views/post_like.py
@@ -1,5 +1,9 @@
+import json
+
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
 from members.models.auth import User
 from ..models import PostPicture
@@ -16,4 +20,27 @@ def post_like(request, pk):
     else:
         post.post_likes.add(user)
 
-    return HttpResponse(str(post.total_likes()))
+    return redirect('posts:post-list')
+
+
+@login_required
+@require_POST
+def post_like_ajax(request):
+    pk = request.POST.get('pk', )
+    post = get_object_or_404(PostPicture, pk=pk)
+    user = User.objects.get(pk=request.user.id)
+
+    if post.post_likes.all().filter(pk=user.id):
+        post.post_likes.remove(user)
+        message = "좋아요 취소"
+
+    else:
+        post.post_likes.add(user)
+        message = "좋아요"
+
+    context = {
+        'like_count': post.like_count,
+        'message': message,
+    }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
