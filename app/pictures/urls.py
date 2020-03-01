08ea22b00a7b15daf1ae5716cb62fc34fd02d5d4
mingyu <rlaalsrbgk@gmail.com>
2020-03-01 21:56:19 +0900

    post like 구현

--- app/pictures/urls.py
+++ app/pictures/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import post_list, post_like
+from .views import post_list, post_like, post_like_ajax
 
 app_name = 'posts'
 urlpatterns = [
     path('', post_list, name='post-list'),
-    path('<int:pk>/like', post_like, name='post-like'),
+    path('<int:pk>/like/', post_like, name='post-like'),
+    path('like/', post_like_ajax, name='post-like-ajax'),
 
 ]
