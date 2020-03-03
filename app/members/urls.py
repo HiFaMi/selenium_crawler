c661dc8cc02fd6c1892fa210ec6806f1f835dbeb
mingyu <rlaalsrbgk@gmail.com>
2020-03-03 18:22:01 +0900

    profile url 추가

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views.profile import user_profile
 from .views.auth_logout import user_logout
 from .views.auth_login import user_login
 from .views.auth_signup import signup
@@ -9,4 +10,5 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('profile/', user_profile, name='profile'),
 ]
