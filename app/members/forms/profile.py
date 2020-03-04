676a4bd72d0982d87b317d25ee14371fd675f79d
mingyu <rlaalsrbgk@gmail.com>
2020-03-04 14:41:49 +0900

    profile form 구현

--- app/members/forms/profile.py
+++ app/members/forms/profile.py
@@ -25,11 +25,20 @@ class ProfileForm(forms.ModelForm):
             'new_password_check',
         )
 
+    def clean_new_password(self):
+        data = self.cleaned_data['new_password']
+
+        if len(data) < 8:
+            self.add_error('new_password', "비밀번호는 최소 8글자 이상이여야 합니다.")
+
+        return data
+
     def clean(self):
         super().clean()
         new_password = self.cleaned_data['new_password']
         check_password = self.cleaned_data['new_password_check']
 
         if new_password != check_password:
-            self.add_error(new_password, "두 비밀번호가 같지 않습니다.")
-            self.add_error(check_password, "두 비밀번호가 같지 않습니다.")
+            self.add_error('new_password_check', "두 비밀번호가 같지 않습니다.")
+
+        return self.cleaned_data
