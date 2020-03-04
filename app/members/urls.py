97dea10c289b250c03b321905d1cc84433a5a197
mingyu <rlaalsrbgk@gmail.com>
2020-03-04 14:44:06 +0900

    profile, user like post url 추가

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views.profile import user_profile
+from .views.profile import user_profile, django_profile, user_like
 from .views.auth_logout import user_logout
 from .views.auth_login import user_login
 from .views.auth_signup import signup
@@ -11,4 +11,8 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('profile/', user_profile, name='profile'),
+
+    path('profile_test/', django_profile, name='django-profile'),
+
+    path('like-post/', user_like, name='user-like'),
 ]
