b515c139ee13174e94b340fb0a407140032825a3
mingyu <rlaalsrbgk@gmail.com>
2020-03-08 22:58:07 +0900

    api signup url 추가

--- app/members/urls/api.py
+++ app/members/urls/api.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from ..api.signup import ApiSignUp
 from ..api.login import AuthToken
 
 app_name = 'members-api'
 urlpatterns = [
     path('login/', AuthToken.as_view(), name='login'),
+    path('signup/', ApiSignUp.as_view(), name='signup'),
 
 ]
