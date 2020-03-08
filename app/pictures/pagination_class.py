11724fa35107bb36210f549f76c4c7b6ba338c9a
mingyu <rlaalsrbgk@gmail.com>
2020-03-08 16:51:34 +0900

    APIView pagination class

--- app/pictures/pagination_class.py
+++ app/pictures/pagination_class.py
@@ -11,3 +11,28 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 21
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class PaginationHandlerMixin(object):
+
+    @property
+    def paginator(self):
+        if not hasattr(self, '_paginator'):
+            if self.pagination_class is None:
+                self._paginator = None
+
+            else:
+                self._paginator = self.pagination_class()
+
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+
+        if self.paginator is None:
+            return None
+
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self, data):
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
