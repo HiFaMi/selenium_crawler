915af44f4596ab20fa9671a90db08d4db3fb6361
mingyu <rlaalsrbgk@gmail.com>
2020-03-08 21:15:20 +0900

    token authentication 추가

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -64,11 +64,18 @@ INSTALLED_APPS = [
     'pictures.apps.PicturesConfig',
 
     'rest_framework',
+    'rest_framework.authtoken',
+
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+
 }
 
 MIDDLEWARE = [
