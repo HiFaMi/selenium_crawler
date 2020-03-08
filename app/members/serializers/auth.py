707d8f6c47dc7b5f236637f8450c54a01320ab98
mingyu <rlaalsrbgk@gmail.com>
2020-03-08 21:13:32 +0900

    serializer token login 구현

--- app/members/serializers/auth.py
+++ app/members/serializers/auth.py
@@ -1,15 +1,24 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = (
-            'pk',
             'username',
-            'user_email'
+            'user_email',
+            'like',
         )
+
+
+class UserAuthSerializer(AuthTokenSerializer):
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists() is False:
+            raise serializers.ValidationError("없는 사용자 입니다.")
+        return value
+
