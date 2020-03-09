63a01ab114cbf5f25c60cdba678999362e46c315
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 17:52:59 +0900

    generic signup url 추가

--- app/members/urls/api.py
+++ app/members/urls/api.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ..api.delete import AuthDelete
 from ..api.logout import AuthLogout
 from ..api.profile import AuthProfile, AuthProfileGeneric
-from ..api.signup import ApiSignUp
+from ..api.signup import ApiSignUp, SignUpGeneric
 from ..api.login import AuthToken, AuthTokenGeneric
 
 app_name = 'members-api'
@@ -15,6 +15,7 @@ urlpatterns = [
     path('logout/', AuthLogout.as_view(), name='api-logout'),
 
     path('signup/', ApiSignUp.as_view(), name='api-signup'),
+    path('signup_generic/', SignUpGeneric.as_view(), name='api-signup-generic'),
 
     path('profile/', AuthProfile.as_view(), name='api-profile'),
     path('profile_generic/', AuthProfileGeneric.as_view(), name='api-profile-generic'),
