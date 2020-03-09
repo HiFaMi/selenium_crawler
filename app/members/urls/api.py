1d974be6b4537af7124c4a08a9bf04c73d72aaf1
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 15:57:33 +0900

    APIview, generic view url 추가

--- app/members/urls/api.py
+++ app/members/urls/api.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
+from ..api.profile import AuthProfile, AuthProfileGeneric
 from ..api.signup import ApiSignUp
-from ..api.login import AuthToken
+from ..api.login import AuthToken, AuthTokenGeneric
 
 app_name = 'members-api'
 urlpatterns = [
-    path('login/', AuthToken.as_view(), name='login'),
-    path('signup/', ApiSignUp.as_view(), name='signup'),
+
+    path('login/', AuthToken.as_view(), name='api-login'),
+    path('login_generic/', AuthTokenGeneric.as_view(), name='api-login-generic'),
+
+    path('signup/', ApiSignUp.as_view(), name='api-signup'),
+
+    path('profile/', AuthProfile.as_view(), name='api-profile'),
+    path('profile_generic/', AuthProfileGeneric.as_view(), name='api-profile-generic'),
+
 
 ]
