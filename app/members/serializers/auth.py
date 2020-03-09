711b60d2e82fff1eca5035ea3086f51ce8fc8fc7
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 15:56:56 +0900

    UserAuthSerializer validate 추가

--- app/members/serializers/auth.py
+++ app/members/serializers/auth.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 
@@ -16,9 +16,23 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserAuthSerializer(AuthTokenSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password'
+        )
 
     def validate_username(self, value):
         if User.objects.filter(username=value).exists() is False:
             raise serializers.ValidationError("없는 사용자 입니다.")
         return value
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("not authenticated user")
+
+        return user
+
 
