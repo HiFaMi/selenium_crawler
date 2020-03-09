8d1f8c658bf3bf67a710f69f3b87b1e6a8eab123
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 17:52:10 +0900

    api generic view signup 구현

--- app/members/api/signup.py
+++ app/members/api/signup.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..serializers.signup import SignupSerializer
 
 User = get_user_model()
+
 
 class ApiSignUp(APIView):
 
@@ -21,3 +22,15 @@ class ApiSignUp(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignUpGeneric(generics.CreateAPIView):
+    serializer_class = SignupSerializer
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        instance.set_password(instance.password)
+        instance.save()
+
+    def get_object(self):
+        return self.request.user
