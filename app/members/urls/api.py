81f5494da915d3793e3bb0691f87c63aceecc25f
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 16:27:00 +0900

    user logout url 추가

--- app/members/urls/api.py
+++ app/members/urls/api.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from ..api.logout import AuthLogout
 from ..api.profile import AuthProfile, AuthProfileGeneric
 from ..api.signup import ApiSignUp
 from ..api.login import AuthToken, AuthTokenGeneric
@@ -9,6 +10,8 @@ urlpatterns = [
 
     path('login/', AuthToken.as_view(), name='api-login'),
     path('login_generic/', AuthTokenGeneric.as_view(), name='api-login-generic'),
+
+    path('logout/', AuthLogout.as_view(), name='api-logout'),
 
     path('signup/', ApiSignUp.as_view(), name='api-signup'),
 
