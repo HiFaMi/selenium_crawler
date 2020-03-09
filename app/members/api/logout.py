ce475a5c6fbec15ce4f72bf1d47bf1ea05037798
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 16:27:22 +0900

    user logout api 구현

---
+++ app/members/api/logout.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions, status, generics
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..serializers.auth import UserSerializer
+
+
+class AuthLogout(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+
+        token = Token.objects.get(user=request.user)
+
+        token.delete()
+
+        data = {
+            'alter': "로그아웃 되셨습니다."
+        }
+
+        return Response(data, status=status.HTTP_204_NO_CONTENT)
+
+
