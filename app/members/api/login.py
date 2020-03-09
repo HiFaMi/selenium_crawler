2c34b5cf85d79c10eb63794af8cef14fb8281c59
mingyu <rlaalsrbgk@gmail.com>
2020-03-09 15:56:17 +0900

    generic view를 이용한 authentication 추가

--- app/members/api/login.py
+++ app/members/api/login.py
@@ -1,6 +1,7 @@
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from ..serializers.auth import UserAuthSerializer, UserSerializer
 
@@ -12,13 +13,33 @@ class AuthToken(APIView):
 
         serializer.is_valid(raise_exception=True)
 
-        user = serializer.validated_data['user']
+        user = serializer.validated_data
 
         token, _ = Token.objects.get_or_create(user=user)
 
         data = {
             'token': token.key,
             'user': UserSerializer(user).data
+        }
+
+        return Response(data)
+
+
+class AuthTokenGeneric(generics.GenericAPIView):
+    serializer_class = UserAuthSerializer
+
+    def post(self, request):
+        serializer = UserAuthSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data
+
+        token, _ = Token.objects.get_or_create(user=user)
+
+        data = {
+            'token-key': token.key,
+            'user': UserSerializer(user, context=self.get_serializer_context()).data
         }
 
         return Response(data)
