270edf39b1b950979b304feebf9113fad11be651
mingyu <rlaalsrbgk@gmail.com>
2020-03-10 15:17:55 +0900

    api post like url 추가

--- app/pictures/urls/api.py
+++ app/pictures/urls/api.py
@@ -1,9 +1,16 @@
 from django.urls import path
+
+from pictures.apis.post_detail import PostDetail
+from ..apis.post_like import PostLike
 from ..apis.post_list_api import PostList, PostListPagination
 
 app_name = 'posts-api'
 urlpatterns = [
-    path('list/', PostList.as_view(), name='post-list'),
-    path('list-pagination/', PostListPagination.as_view(), name='post-list-pagination'),
+    path('list/', PostList.as_view(), name='api-post-list'),
+    path('list-pagination/', PostListPagination.as_view(), name='api-post-list-pagination'),
+
+    path('list/<int:pk>/', PostDetail.as_view(), name='api-post-detail'),
+
+    path('list/<int:pk>/like/', PostLike.as_view(), name='api-post-like'),
 
 ]
