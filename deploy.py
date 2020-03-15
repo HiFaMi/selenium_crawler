93f19551c105f797fa32620fbc6bf9be1ccc3c4f
mingyu <rlaalsrbgk@gmail.com>
2020-03-15 16:50:50 +0900

    root pwd 자동입력 구현

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ import subprocess
 import argparse
 import sys
 
-MODES = ['kill', 'deploy', 'base', 'maker']
+MODES = ['kill', 'deploy', 'base', 'maker', 'oneclick']
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_DIR = os.path.join(ROOT_DIR, '.secret')
@@ -16,6 +16,7 @@ secrets = json.load(open(os.path.join(SECRET_DIR, 'secrets.json')))
 
 AWS_KEY_ROOT = secrets['AWS']['AWS_ACCESS_KEY_ROOT']
 AWS_URL_CONNECT = secrets['AWS']['AWS_URL_CONNECT']
+ROOT_PASSWORD = secrets['ROOT']['PASSWORD']
 
 AWS_CONNECT = 'ssh -i {} {}'.format(AWS_KEY_ROOT, AWS_URL_CONNECT)
 AWS_SCP_FILE = 'scp -i {}'.format(AWS_KEY_ROOT)
@@ -46,8 +47,13 @@ def mode_function(mode):
 
 
 def build_kill():
-    subprocess.call("{} sudo docker stop $(sudo docker ps -q)".format(AWS_CONNECT), shell=True)
-    print("Docker stop success")
+    command = subprocess.check_output("{} sudo docker ps -q".format(AWS_CONNECT), shell=True)
+    if bool(command) is True:
+        print("One docker process is activated\nkill docker process")
+        subprocess.call("{} sudo docker stop {}".format(AWS_CONNECT, command.decode('utf-8')[:-2]), shell=True)
+        print("kill")
+    else:
+        print("None docker process")
 
 
 def build_deploy():
@@ -66,12 +72,18 @@ def build_maker():
     try:
         subprocess.call('poetry export -f requirements.txt > requirements.txt', shell=True)
         subprocess.call("{} sudo rm -rf project".format(AWS_CONNECT), shell=True)
-        subprocess.call("sudo {} -r ~/project/poetry_test/ {}:".format(AWS_SCP_FILE, AWS_URL_CONNECT), shell=True)
+        subprocess.call("echo {} | sudo -S {} -r ~/project/poetry_test/ {}:".format(ROOT_PASSWORD, AWS_SCP_FILE, AWS_URL_CONNECT), shell=True)
         subprocess.call("{} sudo mv /home/ubuntu/poetry_test /home/ubuntu/project".format(AWS_CONNECT), shell=True)
         print('Rename to project')
 
     finally:
         os.remove('requirements.txt')
+
+
+def build_oneclick():
+    build_kill()
+    build_maker()
+    build_deploy()
 
 
 if __name__ == '__main__':
