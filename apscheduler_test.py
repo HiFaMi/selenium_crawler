80bccffbc3bc6f414c877f181100f858b9105e22
mingyu <rlaalsrbgk@gmail.com>
2020-03-16 11:56:54 +0900

    매달 15일 새벽2시에 크롤링하도록 수정

--- apscheduler_test.py
+++ apscheduler_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import datetime
+import sys
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -7,21 +8,25 @@ import time
 from selenium_twitter import twitter_crawler
 
 
-# test
-def job():
-    print("I'm working.... | [time] {}:{}:{}".format(time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec))
+def job_crawler():
+    while True:
+        try:
+            twitter_crawler()
+            f = open('crawler_date.txt', 'a')
+            f.write("Run crawler date = {}-{}\n".format(time.localtime().tm_mon, time.localtime().tm_yday))
+            f.write("Run crawler time = {}:{}:{}".format(time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec))
+
+            break
+
+        except:
+            print('try again......')
+            print(sys.exc_info()[0])
 
 
-# download = twitter_crawler()
+if __name__ == '__main__':
 
-
-sched = BackgroundScheduler()
-sched.start()
-
-# sched.add_job(job, 'interval', seconds=3, id='test_1')
-sched.add_job(twitter_crawler, 'cron',  hour=1, id='test_1', next_run_time=datetime.datetime.now())
-
-
-f = open('crawler_date.txt', 'a')
-f.write("Run crawler date = {}-{}\n".format(time.localtime().tm_mon, time.localtime().tm_yday))
-f.write("Run crawler time = {}:{}:{}".format(time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec))
+    sched = BackgroundScheduler()
+    sched.start()
+    sched.add_job(job_crawler, 'corn', day=15, hour=2, id='test_1', start_date=datetime.datetime.now())
+    input("Press enter to exit.")
+    sched.shutdown()
