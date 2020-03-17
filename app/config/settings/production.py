9a99bf9c1059a5bc78b7da099b67c9a5c146a8a8
mingyu <rlaalsrbgk@gmail.com>
2020-03-17 17:22:28 +0900

    static, media url production용

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -3,13 +3,32 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'localhost',
     '*.amazonaws.com',
     'pby.kr',
     'parkboyoung.kr',
 ]
 
 WSGI_APPLICATION = 'config.wsgi.production.application'
+
+# s3 setting
+AWS_REAGION = "ap-northeasr-2"
+AWS_STORAGE_BUCKET_NAME = 'selenium-crawler'
+
+AWS_QUERYSTRING_AUTH = False
+
+AWS_ACCESS_KEY_ID = secrets['AWS']['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = secrets['AWS']['AWS_SECRET_ACCESS_KEY']
+AWS_LOCATION = '.static/'
+
+AWS_S3_HOST = f's3.{AWS_REAGION}.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_MEDIA_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REAGION}.amazonaws.com'
+
+STATIC_URL = f"http://{AWS_S3_CUSTOM_DOMAIN}/.static/"
+STATICFILES_STORAGE = 'config.storage_backend.StaticStorage'
+
+MEDIA_URL = f"http://{AWS_S3_MEDIA_DOMAIN}/.media/"
+DEFAULT_FILE_STORAGE = 'config.storage_backend.MediaStorage'
 
 # Database
 
