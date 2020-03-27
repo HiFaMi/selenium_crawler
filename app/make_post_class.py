6b96619a28d63ca3f31e29d3ca6147e108ac8432
mingyu <rlaalsrbgk@gmail.com>
2020-03-27 17:29:59 +0900

    add s3 to facebook crawler

--- app/make_post_class.py
+++ app/make_post_class.py
@@ -1,38 +1,66 @@
 #!/usr/bin/env python
 
 import os
+
+import boto3
 import django
 django.setup()
 
+from config.settings.base import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from pictures.models import PostPicture
 
 PRESENT_DIR = os.path.abspath('')
 IMG_DIR = os.path.join(PRESENT_DIR, '.media/img')
 
-dir_lists = os.listdir(IMG_DIR)
+
+def local_to_twitter_crawler_class():
+    dir_lists = os.listdir(IMG_DIR)
+    for dir_list in dir_lists:
+        if os.path.isdir(os.path.join(IMG_DIR, dir_list)):
+            class_user_name = dir_list
+            img_lists = os.listdir(os.path.join(IMG_DIR, dir_list))
+            new_img_counter = 0
+            for img_list in img_lists:
+                if PostPicture.objects.filter(post_picture='img/{}/{}'.format(class_user_name, img_list)).exists() is False:
+                    post = PostPicture.objects.create(
+                        post_user=class_user_name,
+                        post_picture='img/{}/{}'.format(class_user_name, img_list)
+                    )
+                    post.save()
+
+                    post = PostPicture.objects.last()
+                    post.post_modal_target = "PostModalTarget{}".format(post.id)
+                    post.save()
+
+                    new_img_counter += 1
+
+            if new_img_counter == 0:
+                print("nothing new img about {}".format(dir_list))
+            else:
+                print("new img about {}: +{}".format(dir_list, new_img_counter))
+    print("Done")
 
 
-for dir_list in dir_lists:
-    if os.path.isdir(os.path.join(IMG_DIR, dir_list)):
-        class_user_name = dir_list
-        img_lists = os.listdir(os.path.join(IMG_DIR, dir_list))
-        new_img_counter = 0
-        for img_list in img_lists:
-            if PostPicture.objects.filter(post_picture='img/{}/{}'.format(class_user_name, img_list)).exists() is False:
-                post = PostPicture.objects.create(
-                    post_user=class_user_name,
-                    post_picture='img/{}/{}'.format(class_user_name, img_list)
+def s3_to_facebook_crawler_make_class():
+    session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    s3 = session.resource('s3')
+    bucket = s3.Bucket('selenium-crawler')
+
+    new_post = 0
+
+    for obj in bucket.objects.filter(Delimiter='/', Prefix='.media/img/facebook/'):
+        path = obj.key.split('/')[-1]
+
+        if path:
+            if not PostPicture.objects.filter(post_picture=f'img/facebook/{path}').exists():
+                PostPicture.objects.create(
+                    post_user='facebook',
+                    post_picture=f'img/facebook/{path}'
                 )
-                post.save()
-
                 post = PostPicture.objects.last()
-                post.post_modal_target = "PostModalTarget{}".format(post.id)
+                post.post_modal_target = post.id
                 post.save()
 
-                new_img_counter += 1
+                new_post += 1
 
-        if new_img_counter == 0:
-            print("nothing new img about {}".format(dir_list))
-        else:
-            print("new img about {}: +{}".format(dir_list, new_img_counter))
-print("Done")
+    print(f'create {new_post} new post class')
--- selenium_facebook.py
+++ selenium_facebook.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium_crawling import facebook_login, download_facebook_image
+from selenium_crawling import facebook_login, download_facebook_image, download_facebook_image_to_s3
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SECRET_DIR = os.path.join(ROOT_DIR, ".secret")
@@ -67,12 +67,60 @@ def selenium_facebook():
     driver.quit()
 
 
-if __name__ == "__main__":
+def selenium_facebook_to_s3():
+    driver = facebook_login(email=email, password=password)
+
+    driver.implicitly_wait(10)
+
+    try:
+        search_box = driver.find_element_by_name("q")
+        search_box.send_keys("#박보영")
+        driver.find_element_by_css_selector("#js_c > form > button").click()
+
+        driver.maximize_window()
+
+        element = driver.find_element_by_css_selector(
+            """#xt_uniq_2 > div._77we > div > div._6v_a > div._6v-_ > div._6v_0._4ik4._4ik5 > a"""
+        )
+        element.click()
+
+        wait = WebDriverWait(driver, 10)
+        move_picture_post = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, """#u_fetchstream_2_4 > div:nth-child(3) > a""")))
+        move_picture_post.click()
+
+        driver.find_element_by_xpath("""//*[@id="js_1z"]/div/div[2]/div[2]/div[1]/a[1]/div/div[1]""").click()
+        driver.find_element_by_xpath("""//*[@id="2762459600649210"]/a/img""").click()
+
+        count = 0
+        while True:
+            time.sleep(2)
+            picture = driver.find_element_by_css_selector("div > div > img.spotlight")
+            src = picture.get_attribute('src')
+
+            download = download_facebook_image_to_s3(src=src, aws_access_key=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+            if download is False:
+                break
+
+            count += 1
+            driver.find_element_by_css_selector("a.next").click()
+
+        print(f'{count} images downloaded')
+
+    finally:
+        driver.quit()
+
+
+def run_selenium_facebook():
     while True:
         try:
-            selenium_facebook()
+            selenium_facebook_to_s3()
             break
 
         except:
             print('Try again...')
 
+
+if __name__ == "__main__":
+    selenium_facebook_to_s3()
