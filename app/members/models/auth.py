c871318b80ae51b1807897234dd69644660056ba
mingyu <rlaalsrbgk@gmail.com>
2020-04-07 17:25:08 +0900

    social choice field 구현

--- app/members/models/auth.py
+++ app/members/models/auth.py
@@ -4,5 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
 
+    SOCIAL_CHOICES = (
+        ('L', 'local'),
+        ('K', 'kakao'),
+        ('D', 'discord'),
+        ('GG', 'google'),
+        ('GH', 'github'),
+    )
+
     username = models.CharField(max_length=50, unique=True)
     user_email = models.EmailField(verbose_name="이메일")
+    social = models.CharField(max_length=2, choices=SOCIAL_CHOICES, default='L')
