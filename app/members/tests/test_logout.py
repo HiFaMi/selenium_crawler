30243db162a38269d20e077e708c7810ea60adcf
mingyu <rlaalsrbgk@gmail.com>
2020-04-17 20:54:56 +0900

    add logout api test

---
+++ app/members/tests/test_logout.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+User = get_user_model()
+
+
+class LogoutTest(TestCase):
+
+    def test_logout(self):
+        client = APIClient()
+        User.objects.create_user(
+            username='test',
+            user_email='test@gmail.com',
+            password='testpassword'
+        )
+
+        user = User.objects.first()
+
+        token, _ = Token.objects.get_or_create(user=user)
+
+        client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
+        response = client.post(reverse('members-api:api-logout'))
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.data, {'alter': "로그아웃 되셨습니다."})
