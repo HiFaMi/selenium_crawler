c907524adc588a4b220811d44811c2c1769ca6b6
mingyu <rlaalsrbgk@gmail.com>
2020-04-21 17:54:52 +0900

    superuser status에 따라서 django admin color 결정

--- app/members/admin.py
+++ app/members/admin.py
@@ -6,9 +6,17 @@ User = get_user_model()
 
 
 class UserAdmin(SortableModelAdmin):
-    list_display = ['username', 'user_email', 'is_superuser', 'order']
-    list_editable = ('order',)
+    list_display = ['username', 'user_email', 'is_superuser', 'social', 'order']
+    list_editable = ('is_superuser',)
     sortable = 'order'
+
+    def suit_row_attributes(self, obj):
+        css_class = {
+            '1': 'success',
+            '0': 'warning',
+        }.get(obj.is_superuser_status)
+        if css_class:
+            return {'class': css_class, 'data': obj.is_superuser_status}
 
 
 admin.site.register(User, UserAdmin)
--- app/members/models/auth.py
+++ app/members/models/auth.py
@@ -12,7 +12,14 @@ class User(AbstractUser):
         ('GH', 'github'),
     )
 
-    username = models.CharField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True, verbose_name='아이디')
     user_email = models.EmailField(verbose_name="이메일")
     social = models.CharField(max_length=2, choices=SOCIAL_CHOICES, default='L')
     order = models.PositiveIntegerField(null=True)
+
+    @property
+    def is_superuser_status(self):
+        if self.is_superuser:
+            return '1'
+        else:
+            return '0'
